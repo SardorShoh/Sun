6daf630c1b6cbaaeddf6c9424ff47ef5e9f74353
Sardor <41294422+SardorShoh@users.noreply.github.com>
2021-01-18 00:05:48 +0500

    commit

--- lib/Response.php
+++ lib/Response.php
@@ -5,17 +5,19 @@ use \lib\Headers;
 
 class Response {
 
-  public function json($arr) : void {
+  //data encode to JSON and send to client
+  public function json($data) : void {
     Headers::set('Content-Type', 'application/json;charset=utf-8');
-    echo json_encode($arr);
-    
+    echo json_encode($data);
   }
 
+  //send string data to the client 
   public function sendString($res) : void {
     Headers::set('Content-Type', 'text/plain;charset=utf-8');
     echo (string)$res;
   }
 
+  //send html data to the client
   public function html($html) : void {
     Headers::set('Content-Type', 'text/html');
     echo $html;
