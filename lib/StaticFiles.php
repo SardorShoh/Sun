fca1ddfcde2b5e2bdaf55f5d7c9f96bf33a75e3c
Sardor <41294422+SardorShoh@users.noreply.github.com>
2021-07-01 10:37:15 +0500

    commit

---
+++ lib/StaticFiles.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace lib;
+
+class StaticFiles {
+  
+  static public function serve_dir(string $path) {
+
+  }
+
+  static public function server_file(string $path) {
+    
+  }
+
+}
